879f5beb5f8c0bd8485fdce49172d8f9eef745cc
Firewire9941 <54448369+Firewire9941@users.noreply.github.com>
2022-05-05 21:11:30 +0200

    Added option to generate mares

--- common/defines/01_defines.lua
+++ common/defines/01_defines.lua
@@ -6,3 +6,14 @@ NDefines.NGame.END_DATE = "1000.1.1.1"
 
 -- Fuel
 NDefines.NGame.FUEL_RESOURCE = "wheat"							-- resource that will give country fuel
+
+NCountry = {
+    FEMALE_UNIT_LEADER_BASE_CHANCE = {
+		-- applies as a factor to female unit leader randomization
+		-- the values needs to be zero if you don't actually have random portraits
+		1.0, -- country leaders
+		1.0, -- army leaders
+		1.0, -- navy leaders
+		1.0, -- air leaders
+		1.0, -- operatives
+	},
